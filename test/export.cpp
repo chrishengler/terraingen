538e952eb43933866d18de03f649ff32a9045d8b
Chris Hengler <chris.hengler@evernest.com>
2024-09-10 20:12:38 +0200

    test exporting all max

--- test/export.cpp
+++ test/export.cpp
@@ -23,15 +23,11 @@ std::filesystem::path get_temp_filepath(const std::string &extension)
 
 TEST_CASE("export png")
 {
-    Heightmap testHeightmap;
-    for(int x=0; x<10; x++)
-    {
-        auto row = std::vector<unsigned int>(10);
-        testHeightmap.push_back(row);
-    }
-
     SECTION("image exporter writes a file")
     {
+        Heightmap testHeightmap;
+        testHeightmap.push_back(std::vector<unsigned int>(1));
+
         ImageExporter imgExporter;
         auto filepath = get_temp_filepath("png");
         imgExporter.saveToFile(testHeightmap, filepath);
@@ -42,6 +38,13 @@ TEST_CASE("export png")
     {
         SECTION("all 0s")
         {
+            Heightmap testHeightmap;
+            for(int x=0; x<10; x++)
+            {
+                auto row = std::vector<unsigned int>(10);
+                testHeightmap.push_back(row);
+            }
+
             ImageExporter imgExporter;
             auto filepath = get_temp_filepath("png");
             imgExporter.saveToFile(testHeightmap, filepath);
@@ -54,10 +57,39 @@ TEST_CASE("export png")
             boost::gil::read_and_convert_image(exampleFilepath, exampleImage, boost::gil::png_tag());
             boost::gil::gray16_image_t writtenImage;
             boost::gil::read_and_convert_image(filepath, writtenImage, boost::gil::png_tag());
-            REQUIRE(exampleImage.width() == writtenImage.width());
             REQUIRE(exampleImage == writtenImage);
         }
+
+        SECTION("all 1s")
+        {
+            Heightmap testHeightmap;
+            for(int x=0; x<10; x++)
+            {
+                auto row = std::vector<unsigned int>(10);
+                for(int y=0; y<10; y++)
+                {
+                    row[y]=1;
+                }
+                testHeightmap.push_back(row);
+            }
+
+            ImageExporter imgExporter;
+            auto filepath = get_temp_filepath("png");
+            imgExporter.saveToFile(testHeightmap, filepath);
+            REQUIRE(std::filesystem::exists(filepath));
+
+            auto exampleFilepath = std::filesystem::current_path() / "data/black-10x10.png";
+            REQUIRE(std::filesystem::exists(exampleFilepath));
+
+            boost::gil::gray16_image_t exampleImage;
+            boost::gil::read_and_convert_image(exampleFilepath, exampleImage, boost::gil::png_tag());
+            boost::gil::gray16_image_t writtenImage;
+            boost::gil::read_and_convert_image(filepath, writtenImage, boost::gil::png_tag());
+            REQUIRE(exampleImage == writtenImage);
+
+        }
     }
+
 
     std::ifstream expectedFile(std::filesystem::current_path().parent_path() / "test/data/black-10x10.png", std::ios::binary);
 
