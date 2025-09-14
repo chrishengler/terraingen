4092f5934341fbd2a6afb13c2512f5634d0242d9
Chris Hengler <chrysics@gmail.com>
2025-09-14 09:46:23 +0200

    export 8-bit png

--- lib-terraingen/export/imageExporter.cpp
+++ lib-terraingen/export/imageExporter.cpp
@@ -10,7 +10,12 @@ double get_max_value(Image& image) {
 // for now just does pngs with 0 - 255
 void ImageExporter::saveToFile(const Heightmap &terrain, const std::string &filepath)
 {
-    auto flattened_heightmap = flatten_heightmap_uint(terrain, 255);
+    auto flattened_heightmap = flatten_heightmap_uchar(terrain);
     auto dims = getDimensions(terrain);
     stbi_write_png(filepath.c_str(), dims.x, dims.y, 1, flattened_heightmap.data(), 0);
+}
+
+void saveToFile(const std::unique_ptr<Heightmap> &terrain, const std::string &filepath){
+    ImageExporter exporter;
+    return exporter.saveToFile(*terrain, filepath);
 }
